9adefa5c017ad23f720e88ea6fa6adaf816c051f
unknown <spremic.katarina@gmail.com>
2023-11-10 20:30:18 +0100

    Change light actuator to toggle with key press

--- simulation/actuators/light.py
+++ simulation/actuators/light.py
@@ -1,3 +1,6 @@
+import time
+import keyboard
+
 # def turn_on_off(actuator_settings, on_off):
 # """
 #     Works with console input of command to turn light on/off.
@@ -16,19 +19,47 @@
 #         if status != None:
 #             print(f"Light has become {on_off}")
 
+def on_key_event_wrapper(callback, settings):
+    """
+        Sets keyboard hook to register 'l' to turn light on and off.
+    """
+    def on_key_event(event):
+        nonlocal settings
+
+        import RPi.GPIO as GPIO
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(settings['pin'], GPIO.OUT)
+        high_low = GPIO.HIGH if settings['on'] else GPIO.LOW
+        GPIO.output(settings['pin'], high_low)
+
+        if event.event_type == keyboard.KEY_DOWN and event.name == 'l':
+
+            if GPIO.input(settings['pin']) == GPIO.HIGH:
+                time.sleep(0.2)
+                GPIO.output(settings['pin'], GPIO.LOW)
+            if GPIO.input(settings['pin']) == GPIO.LOW:
+                time.sleep(0.2)
+                GPIO.output(settings['pin'], GPIO.HIGH)
+            time.sleep(0.1)
+            callback('on' if GPIO.input(settings['pin']) == GPIO.HIGH else 'off')
+            time.sleep(1)
+
+    return on_key_event
+
+
 def run_light_loop(settings, callback, stop_event):
     """
-        Sets light to on or off depending on settings.json.
-        In loop starts callback every 10 seconds reporting light status (on or off).
+        Initializes light to on/off depending on settings.json.
+        Sets keyboard hook to listen to key 'l' press to toggle light.
     """
     import RPi.GPIO as GPIO
-    import time
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(settings['pin'], GPIO.OUT)
-    on_off = GPIO.HIGH if settings['on'] else GPIO.LOW
-    GPIO.output(settings['pin'], on_off)
+    high_low = GPIO.HIGH if settings['on'] else GPIO.LOW
+    GPIO.output(settings['pin'], high_low)
+
+    keyboard.hook(on_key_event_wrapper(callback, settings))
     while True:
-        callback('on' if GPIO.input(settings['pin']) == GPIO.HIGH else 'off')
-        time.sleep(10)  
         if stop_event.is_set():
             break
+        time.sleep(1)  
